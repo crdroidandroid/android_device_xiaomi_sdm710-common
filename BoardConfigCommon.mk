400796c7abee25530dd6c2eb512fb2e5d5b97a19
Roger Ellis <ellisr@google.com>
2020-05-13 16:56:10 -0700

    sdm710-common: Allow ELF prebuilts in PRODUCT_COPY_FILES.
     * ELF prebuilts were banned in PRODUCT_COPY_FILES starting
       May 11th. See b/156534160 for long-term fix which should
       override this change.
    
    Change-Id: Icd25b40ca4e4285319775046a1b70de30572be07

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -8,6 +8,8 @@ BOARD_VENDOR := xiaomi
 
 COMMON_PATH := device/xiaomi/sdm710-common
 
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
