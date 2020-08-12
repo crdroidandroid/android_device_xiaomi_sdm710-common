490974680626b82618d864fbdfb265342cd85fc0
Akhil Narang <akhilnarang.1999@gmail.com>
2020-08-12 20:44:21 +0300

    sdm710-common: releasetools: remove unused or unnecessary imports
    Change-Id: Ib6b164182733ccd2ea1d5c885d3746cf14bff397

--- releasetools.py
+++ releasetools.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2009 The Android Open Source Project
 # Copyright (c) 2011, The Linux Foundation. All rights reserved.
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import hashlib
-import common
 import re
 
 def FullOTA_InstallEnd(info):
