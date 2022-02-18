88735ccd7f72c1660782e3aafe312737605d3bd8
Alex Damaratski <alexeydomoratsky1@gmail.com>
2022-02-18 15:56:51 +0000

    Revert "sdm710-common: fingerprint: Uprev to 2.3"
    This reverts commit 93a5d47409c99ad53d38427b1ca07f349b32bc7b.

--- sdm710.mk
+++ sdm710.mk
@@ -190,16 +190,16 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_sdm710 \
-    android.hardware.biometrics.fingerprint@2.3.vendor
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm710
 
 ifeq ($(TARGET_HAS_FOD),true)
 PRODUCT_PACKAGES += \
     libkeymaster_messages.vendor \
-    libkeymaster_portable.vendor
+    libkeymaster_portable.vendor \
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_sdm710
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 endif
 
 # FM
